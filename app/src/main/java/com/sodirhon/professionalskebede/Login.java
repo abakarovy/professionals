dbb7f71052ef6bf1e9489ec8164aab3c5e9a4422
abakarovy <quanoid221@gmail.com>
2024-12-12 11:53:32 +0300

    sdadasda

---
+++ app/src/main/java/com/sodirhon/professionalskebede/Login.java
@@ -0,0 +1,99 @@
+package com.sodirhon.professionalskebede;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Pattern;
+
+public class Login extends AppCompatActivity {
+
+    private Pattern emailPattern = Pattern.compile("\\b[A-Za-z0-9._%-]+@[a-z]+\\.[a-z]{2,6}\\b");
+
+    EditText emailField;
+    EditText passwordField;
+
+    boolean loginValidated = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.log_in);
+
+        Button button = findViewById(R.id.loginButton);
+
+        emailField = findViewById(R.id.emailTextField);
+        passwordField = findViewById(R.id.passwordTextField);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showTermsAndConditionsDialog();
+            }
+        });
+
+//        emailField.addTextChangedListener(new TextValidator(email) {
+//            @Override
+//            public boolean validate(TextView textView, String text) {
+//                if (!emailPattern.matcher(text).find()) {
+//                    email.setError("Email doesn't match");
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+    }
+
+    private void showTermsAndConditionsDialog() {
+        View dialogView = getLayoutInflater().inflate(R.layout.terms_conditions, null);
+        CheckBox checkboxAgree = dialogView.findViewById(R.id.checkbox_agree);
+        TextView textViewTerms = dialogView.findViewById(R.id.textview_terms_conditions);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(Login.this);
+        builder.setTitle("Terms and Conditions")
+                .setView(dialogView)
+                .setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (!checkboxAgree.isChecked()) {
+                            loginValidated = false;
+                        } else {
+                            loginValidated = true;
+                        }
+
+                        if (loginValidated
+                                && emailField != null
+                                && passwordField != null
+                                && validateEmail(emailField.getText().toString())
+                                && passwordField.length() > 6) {
+
+                            System.out.println("Success");
+
+//                            setContentView();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.create().show();
+    }
+
+    private boolean validateEmail(String text) {
+        if (emailPattern.matcher(text).find()) {
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ app/src/main/java/com/sodirhon/professionalskebede/auth/validation/TextValidator.java
@@ -0,0 +1,32 @@
+package com.sodirhon.professionalskebede.auth.validation;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.TextView;
+
+public abstract class TextValidator implements TextWatcher {
+    private final TextView textView;
+
+    public TextValidator(TextView textView) {
+        this.textView = textView;
+    }
+
+    public abstract boolean validate(TextView textView, String text);
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        String text = textView.getText().toString();
+        validate(textView, text);
+    }
+}
